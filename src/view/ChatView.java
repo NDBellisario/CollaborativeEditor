d425cdb36e48677633a42e4c5f015822589cc6f4
Eric William Blitzer <eblitzer@email.arizona.edu>
2015-04-18 13:32:28 -0700

    This is a test push to Blitzer's branch

--- src/view/ChatView.java
+++ src/view/ChatView.java
@@ -6,34 +6,35 @@ import javax.swing.*;
 
 public class ChatView extends JFrame implements Observer
 {
-	private JPanel chatPanel;
-	private JPanel typePanel;
-	private JButton sendButton;
-	private JTextPane groupText, personalTest;
+ private JPanel chatPanel;
+ private JPanel typePanel;
+ private JButton sendButton;
+ private JTextPane groupText, personalTest;
 
-	public ChatView()
-	{
-	}
+ public ChatView()
+ {
+   //Test for sourcetree
+ }
 
-	public void setUpGui()
-	{
-	}
+ public void setUpGui()
+ {
+ }
 
-	@Override
-	public void update(Observable o, Object arg)
-	{
-		// TODO Auto-generated method stub
-	}
-	private class chatSendListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent arg0)
-		{
-		}
-	}
-	private class documentSelect implements ActionListener
-	{
-		public void actionPerformed(ActionEvent arg0)
-		{
-		}
-	}
+ @Override
+ public void update(Observable o, Object arg)
+ {
+  // TODO Auto-generated method stub
+ }
+ private class chatSendListener implements ActionListener
+ {
+  public void actionPerformed(ActionEvent arg0)
+  {
+  }
+ }
+ private class documentSelect implements ActionListener
+ {
+  public void actionPerformed(ActionEvent arg0)
+  {
+  }
+ }
 }

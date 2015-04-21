7712fce4d0162be79dcf9ac8e3589e7a9a2db8d8
Taylor Cox <taylorcox75@gmail.com>
2015-04-20 21:58:41 -0700

    fixed the broken thing

--- src/controller/CEController.java
+++ src/controller/CEController.java
@@ -7,15 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.Socket;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-
 import javax.swing.*;
 
 import model.*;
 import view.*;
-public class CEController extends JFrame
-{
+
+public class CEController extends JFrame {
 
 	/**
 	 * 
@@ -41,30 +38,30 @@ public class CEController extends JFrame
 	private JPanel chatView;
 	private JPanel editView;
 	JDialog serverconnect;
-	public CEController(ChatAssistant theChat, EditAssistant editAs, UserAssistant theUser)
-	{
+
+	public CEController(ChatAssistant theChat, EditAssistant editAs,
+			UserAssistant theUser) {
 		initUserModels();
 		setupGui();
-		
-	}
-	public void serverConnect()
-	{
 
 	}
-	
-	private void initUserModels(){
-		String serverAddress=JOptionPane.showInputDialog("IPAddress");
+
+	public void serverConnect() {
+
+	}
+
+	private void initUserModels() {
+		String serverAddress = JOptionPane.showInputDialog("IPAddress");
 		String port = JOptionPane.showInputDialog("Port ID");
-		String userName=JOptionPane.showInputDialog("Username");
-		String passWord=JOptionPane.showInputDialog("Password");
+		String userName = JOptionPane.showInputDialog("Username");
+		String passWord = JOptionPane.showInputDialog("Password");
 		user = new User(userName, passWord, 2);
 	}
-	
-	private void setupGui() {
-		//Permissions Pop up
 
-		
-		//Initializing graphic user interface variables 
+	private void setupGui() {
+		// Permissions Pop up
+
+		// Initializing graphic user interface variables
 		mainProgPanel = new JPanel();
 		menuBarCore = new JMenuBar();
 		fileContainer = new JMenu("File");
@@ -81,196 +78,188 @@ public class CEController extends JFrame
 		removeUser = new JMenuItem("Remove User");
 		changePermission = new JMenuItem("Permissions Options");
 		showOptions = new JMenuItem("Show Documents");
-		
-		
+
 		this.setTitle("brahhh");
-		
-		//Adding action listeners for File
+
+		// Adding action listeners for File
 		quitOption.addActionListener(new ExitListener());
 		save.addActionListener(new SaveListener());
 		saveLocal.addActionListener(new SaveLocalListener());
-	
-		//Adding Action Listeners for Edit
+
+		// Adding Action Listeners for Edit
 		undo.addActionListener(new UndoListener());
 		redo.addActionListener(new RedoListener());
 		version.addActionListener(new VersionListener());
-		
-		//Adding Action Listener for User
+
+		// Adding Action Listener for User
 		addUser.addActionListener(new AddUserListener());
 		removeUser.addActionListener(new RemoveUserListener());
 		changePermission.addActionListener(new PermissionListener());
-		
-		//Adding Action Listener for Document Editor
+
+		// Adding Action Listener for Document Editor
 		showOptions.addActionListener(new showOptionsListener());
-		
-		//add main menu buttons to bar
+
+		// add main menu buttons to bar
 		menuBarCore.add(fileContainer);
 		menuBarCore.add(editContainer);
 		menuBarCore.add(userContainer);
 		menuBarCore.add(documentContainer);
 
-		//add document menu buttons
+		// add document menu buttons
 		documentContainer.add(showOptions);
-		
-		//fileContainer sub menu buttons
+
+		// fileContainer sub menu buttons
 		fileContainer.add(save);
 		fileContainer.add(saveLocal);
 		fileContainer.add(quitOption);
-		
-		//editContainer sub menu buttons
+
+		// editContainer sub menu buttons
 		editContainer.add(undo);
 		editContainer.add(redo);
 		editContainer.add(version);
-		
-		//userContainer sub menu buttons
+
+		// userContainer sub menu buttons
 		userContainer.add(addUser);
 		userContainer.add(removeUser);
 		userContainer.add(changePermission);
-		
-		//Add menu bar
+
+		// Add menu bar
 		this.setJMenuBar(menuBarCore);
-		
-		
-		//Add ChatView
-		 chatView = new ChatView(user.getUserName());
-		 editView = new EditView(user.getUserName() + " Client");
-		 this.setLayout(new BorderLayout());
-		 this.add(chatView, BorderLayout.EAST);
-		 this.add(editView, BorderLayout.CENTER);
-		 
-	
-		
+
+		// Add ChatView
+		chatView = new ChatView(user.getUserName());
+		editView = new EditView(user.getUserName() + " Client");
+		this.setLayout(new BorderLayout());
+		this.add(chatView, BorderLayout.EAST);
+		this.add(editView, BorderLayout.CENTER);
+
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		//pack and create!
+
+		// pack and create!
 		this.pack();
 		this.setVisible(true);
 	}
-	
 
-	//Listener Private Classes
-	private class ExitListener implements ActionListener{
+	// Listener Private Classes
+	private class ExitListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
 		}
-		
+
 	}
-	
-	private class SaveListener implements ActionListener{
+
+	private class SaveListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	
-	private class SaveLocalListener implements ActionListener{
+
+	private class SaveLocalListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 
-	private class UndoListener implements ActionListener{
+	private class UndoListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 
-	private class RedoListener implements ActionListener{
+	private class RedoListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 
-	private class VersionListener implements ActionListener{
+	private class VersionListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	
-	private class AddUserListener implements ActionListener{
+
+	private class AddUserListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	private class RemoveUserListener implements ActionListener{
+
+	private class RemoveUserListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	private class PermissionListener implements ActionListener{
+
+	private class PermissionListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	
-	private class ServerFirstContact implements Runnable
-	{
 
-		public ServerFirstContact(Socket arg)
-		{
-			
+		}
+
+	}
+
+	private class ServerFirstContact implements Runnable {
+
+		public ServerFirstContact(Socket arg) {
+
 		}
 
 		@Override
-		public void run()
-		{
+		public void run() {
 			// TODO Auto-generated method stub
 		}
 	}
-	private class ServerListener implements Runnable
-	{
 
-		public ServerListener(Socket arg)
-		{
+	private class ServerListener implements Runnable {
+
+		public ServerListener(Socket arg) {
 
 		}
-		
 
 		@Override
-		public void run()
-		{
+		public void run() {
 			// TODO Auto-generated method stub
 		}
 	}
-	private class showOptionsListener implements ActionListener{
-		
-		public void actionPerformed(ActionEvent e){
+
+	private class showOptionsListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
 			String[] arr = new String[7];
-			for(int i=0;i<7;i++){
-				arr[i] = "String"+i;
+			for (int i = 0; i < 7; i++) {
+				arr[i] = "String" + i;
 			}
 			JFrame frame = new JFrame();
 			frame.setLayout(new BorderLayout());
@@ -279,16 +268,14 @@ public class CEController extends JFrame
 			frame.setVisible(true);
 			frame.setResizable(true);
 			frame.pack();
-			frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-			
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		}
-		
+
 	}
-	public static void main(String[] args)
-	{	
+
+	public static void main(String[] args) {
 		CEController CEC = new CEController(null, null, null);
-		
-		
 
 	}
 }
